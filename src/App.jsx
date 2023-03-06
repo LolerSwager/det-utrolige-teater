5de99b777afc1014add084dc5f9d89ad35f84fb8
Jannick Nielsen <jannicktrollenielsen@hotmail.com>
2023-03-06 08:57:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+import Home from "./Pages/Home"
+
+export default function App() {
+    return (
+        <>
+            <header></header>
+            <Home />
+            <footer></footer>
+        </>
+    )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>somthing</h1>
+        </main>
+    )
+}
